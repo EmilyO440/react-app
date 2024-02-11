382aeca91af490b2ddd965341765e5aff9d2cc3d
emo20142email.vccs.edu <emo2014@email.vccs.edu>
2024-02-11 15:56:26 -0500

    ,

---
+++ react-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navigation from './Navigation';
+
+
+ function App() {
+  return <div className = "App">
+    <Navigation/>
+
+
+  </div>
+ }
+
+
+ export default App;
+
+
---
+++ react-app/src/Navigation.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+export default class Navigation extends Component {
+  render() {
+    return (
+    <nav id="navbar">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+    )
+  }
+}
